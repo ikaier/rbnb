1b0eede4bb45715ee3c184aedb8961816da5b35a
zidai Liu <zidai.liu@gmail.com>
2020-08-18 16:36:48 -0400

    new & create implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'cars', to: 'cars#index'
-  get 'cars/:id', to:'cars#show'
   get 'cars/new', to:'cars#new'
+  post 'cars', to:'cars#create'
+  get 'cars/:id', to:'cars#show'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
