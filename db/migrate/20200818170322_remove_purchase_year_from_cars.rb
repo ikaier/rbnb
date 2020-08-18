dc5a4bae0cb1981a8d8fea35b67cf5579b84ff57
Houkai Qian <871789801@qq.com>
2020-08-18 13:28:37 -0400

    fix type in remove migration file

--- db/migrate/20200818170322_remove_purchase_year_from_cars.rb
+++ db/migrate/20200818170322_remove_purchase_year_from_cars.rb
@@ -1,5 +1,5 @@
 class RemovePurchaseYearFromCars < ActiveRecord::Migration[6.0]
   def change
-    remove_column :cars, :Purchase_year, :integer
+    remove_column :cars, :purchase_year, :integer
   end
 end
