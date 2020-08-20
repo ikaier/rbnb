1832dc3e443dad8a0d433f3393806ec8c040c14a
Joyce Martinez <joycetezanos@gmail.com>
2020-08-20 15:51:54 -0500

    Top Categories and Last Offers working with code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Car.create (model: 'Chevrolet Camaro 1969',
+            description: '1969 Pro Touring Chevrolet Camaro with an LS2 6.0-liter engine running close to 600HP. This car is equipped with Wilwood racing brakes and a full Detroit Speed suspension kit. It hugs the ground with the rear 345 tires nestled inside the mini tubbed rear end. It is simply a blast to drive!',
+            price: 269,
+            user_id: 'adriana.mtezanos@gmail.com',
+            category: 'Classics',
+            address: 'Condesa, Mexico City' )
